e14e96102ede6a37958fb06955011f79c3a2bcd6
hikaru555 <32662387+hikaru555@users.noreply.github.com>
2023-07-17 21:18:46 +0000

    change way to develop

---
+++ stripe/checkout-session.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/stripe/stripe-go/v74"
+	"github.com/stripe/stripe-go/v74/checkout/session"
+)
+
+func main() {
+	// This is your test secret API key.
+	stripe.Key = "sk_test_51NLn6uAMAaJjSlCRqP73JO9EnAyybtGXZUUP3g1h61F3sxKai6GAJdu0NxDeHofudqYFFW6DmiHXX7BMoBXq5cIU00bEQrKs2r"
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/create-checkout-session", createCheckoutSession)
+	addr := "hikaru555.github.com/stripe"
+	log.Printf("Listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func createCheckoutSession(w http.ResponseWriter, r *http.Request) {
+	domain := "https://hikaru555.github.com/stripe"
+	params := &stripe.CheckoutSessionParams{
+		LineItems: []*stripe.CheckoutSessionLineItemParams{
+			&stripe.CheckoutSessionLineItemParams{
+				// Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+				Price:    stripe.String("{{PRICE_ID}}"),
+				Quantity: stripe.Int64(1),
+			},
+		},
+		Mode:       stripe.String(string(stripe.CheckoutSessionModePayment)),
+		SuccessURL: stripe.String(domain + "/success.html"),
+		CancelURL:  stripe.String(domain + "/cancel.html"),
+	}
+
+	s, err := session.New(params)
+
+	if err != nil {
+		log.Printf("session.New: %v", err)
+	}
+
+	http.Redirect(w, r, s.URL, http.StatusSeeOther)
+}
