2148af4be9bd411590534824b00fadbf2d59cafc
hikaru555 <32662387+hikaru555@users.noreply.github.com>
2023-07-17 23:48:09 +0000

    change port tp 8080

--- server.go
+++ server.go
@@ -14,13 +14,13 @@ func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("public")))
 	http.HandleFunc("/create-checkout-session", createCheckoutSession)
-	addr := ":4242"
+	addr := ":8080"
 	log.Printf("Listening on %s", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func createCheckoutSession(w http.ResponseWriter, r *http.Request) {
-	domain := ":4242"
+	domain := ":8080"
 	params := &stripe.CheckoutSessionParams{
 		LineItems: []*stripe.CheckoutSessionLineItemParams{
 			/*&stripe.CheckoutSessionLineItemParams*/ {
