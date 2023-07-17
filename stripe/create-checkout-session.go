0d98180d57370be8f2b3fc9cbd31e943d4f75852
hikaru555 <32662387+hikaru555@users.noreply.github.com>
2023-07-17 20:51:26 +0000

    Simplify

--- stripe/create-checkout-session.go
+++ stripe/create-checkout-session.go
@@ -18,49 +18,50 @@ func main() {
 func handleCreateCheckoutSession(w http.ResponseWriter, r *http.Request) {
 	// Create a new Checkout Session
 	/*params := &stripe.CheckoutSessionParams{
-		PaymentMethodTypes: stripe.StringSlice([]string{
-			"card",
-		}),
+	PaymentMethodTypes: stripe.StringSlice([]string{
+		"card",
+	}),
+	LineItems: []*stripe.CheckoutSessionLineItemParams{
+		{
+			PriceData: &stripe.CheckoutSessionLineItemPriceDataParams{
+				Currency: stripe.String("usd"),
+				ProductData: &stripe.CheckoutSessionLineItemPriceDataProductDataParams{
+					Name: stripe.String("Product Name"),
+				},
+				UnitAmount: stripe.Int64(1000), // amount in cents
+			},
+			Quantity: stripe.Int64(1),
+		},
+	},
+	Mode:       stripe.String(string(stripe.CheckoutSessionModePayment)), */
+
+	stripe.Key = "sk_test_51NLn6uAMAaJjSlCRqP73JO9EnAyybtGXZUUP3g1h61F3sxKai6GAJdu0NxDeHofudqYFFW6DmiHXX7BMoBXq5cIU00bEQrKs2r"
+
+	params := &stripe.CheckoutSessionParams{
 		LineItems: []*stripe.CheckoutSessionLineItemParams{
 			{
-				PriceData: &stripe.CheckoutSessionLineItemPriceDataParams{
-					Currency: stripe.String("usd"),
-					ProductData: &stripe.CheckoutSessionLineItemPriceDataProductDataParams{
-						Name: stripe.String("Product Name"),
-					},
-					UnitAmount: stripe.Int64(1000), // amount in cents
-				},
-				Quantity: stripe.Int64(1),
+				Price:    stripe.String("price_H5ggYwtDq4fbrJ"),
+				Quantity: stripe.Int64(2),
 			},
 		},
-		Mode:       stripe.String(string(stripe.CheckoutSessionModePayment)), */
-
-		stripe.Key = "sk_test_51NLn6uAMAaJjSlCRqP73JO9EnAyybtGXZUUP3g1h61F3sxKai6GAJdu0NxDeHofudqYFFW6DmiHXX7BMoBXq5cIU00bEQrKs2r"
-
-		params := &stripe.CheckoutSessionParams{
-  			LineItems: []*stripe.CheckoutSessionLineItemParams{
-    		{
-      			Price: stripe.String("price_H5ggYwtDq4fbrJ"),
-      			Quantity: stripe.Int64(2),
-    		},
-			},
-  			Mode: stripe.String("payment"),
-  			SuccessURL: stripe.String("https://google.com"),
-		}
-		s, _ := session.New(params)
-		// SuccessURL: stripe.String("https://google.com"),
-		// CancelURL:  stripe.String("https://droidsans.com"),
-
+		Mode:       stripe.String("payment"),
+		SuccessURL: stripe.String("https://google.com"),
 	}
+	s, _ := session.New(params)
+	// SuccessURL: stripe.String("https://google.com"),
+	// CancelURL:  stripe.String("https://droidsans.com"),
 
-	/*session, err := session.New(params)
-	if err != nil {
-		log.Printf("Error creating checkout session: %s", err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}*/
-
-	// Return the Checkout Session ID in the response
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"sessionId": "` + session.ID + `"}`))
+	w.Write([]byte(`{"sessionId": "` + s.ID + `"}`))
 }
+
+/*session, err := session.New(params)
+if err != nil {
+	log.Printf("Error creating checkout session: %s", err.Error())
+	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	return
+}*/
+
+// Return the Checkout Session ID in the response
+//w.Header().Set("Content-Type", "application/json")
+//w.Write([]byte(`{"sessionId": "` + session.ID + `"}`))
