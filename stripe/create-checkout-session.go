e9720a34595de13a4e4977e1eb8f18c29aaf4fc9
hikaru555 <32662387+hikaru555@users.noreply.github.com>
2023-07-17 20:45:39 +0000

    make it simple

--- stripe/create-checkout-session.go
+++ stripe/create-checkout-session.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	stripe.Key = "sk_test_51NLn6uAMAaJjSlCRqP73JO9EnAyybtGXZUUP3g1h61F3sxKai6GAJdu0NxDeHofudqYFFW6DmiHXX7BMoBXq5cIU00bEQrKs2r"
+	//stripe.Key = "sk_test_51NLn6uAMAaJjSlCRqP73JO9EnAyybtGXZUUP3g1h61F3sxKai6GAJdu0NxDeHofudqYFFW6DmiHXX7BMoBXq5cIU00bEQrKs2r"
 
 	http.HandleFunc("/create-checkout-session", handleCreateCheckoutSession)
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -17,7 +17,7 @@ func main() {
 
 func handleCreateCheckoutSession(w http.ResponseWriter, r *http.Request) {
 	// Create a new Checkout Session
-	params := &stripe.CheckoutSessionParams{
+	/*params := &stripe.CheckoutSessionParams{
 		PaymentMethodTypes: stripe.StringSlice([]string{
 			"card",
 		}),
@@ -33,17 +33,32 @@ func handleCreateCheckoutSession(w http.ResponseWriter, r *http.Request) {
 				Quantity: stripe.Int64(1),
 			},
 		},
-		Mode:       stripe.String(string(stripe.CheckoutSessionModePayment)),
-		SuccessURL: stripe.String("https://google.com"),
-		CancelURL:  stripe.String("https://droidsans.com"),
+		Mode:       stripe.String(string(stripe.CheckoutSessionModePayment)), */
+
+		stripe.Key = "sk_test_51NLn6uAMAaJjSlCRqP73JO9EnAyybtGXZUUP3g1h61F3sxKai6GAJdu0NxDeHofudqYFFW6DmiHXX7BMoBXq5cIU00bEQrKs2r"
+
+		params := &stripe.CheckoutSessionParams{
+  			LineItems: []*stripe.CheckoutSessionLineItemParams{
+    		{
+      			Price: stripe.String("price_H5ggYwtDq4fbrJ"),
+      			Quantity: stripe.Int64(2),
+    		},
+			},
+  			Mode: stripe.String("payment"),
+  			SuccessURL: stripe.String("https://google.com"),
+		}
+		s, _ := session.New(params)
+		// SuccessURL: stripe.String("https://google.com"),
+		// CancelURL:  stripe.String("https://droidsans.com"),
+
 	}
 
-	session, err := session.New(params)
+	/*session, err := session.New(params)
 	if err != nil {
 		log.Printf("Error creating checkout session: %s", err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
-	}
+	}*/
 
 	// Return the Checkout Session ID in the response
 	w.Header().Set("Content-Type", "application/json")
